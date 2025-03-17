18e74a0524b7006f8fc700f5ec2621f7ad13c53a
Vishal Fenn <vkf635@gmail.com>
2025-03-17 12:35:37 -0400

    Adjusted rear reward weight to 3

--- config_intersection.py
+++ config_intersection.py
@@ -41,10 +41,10 @@ max_speed = 15
 
 #Reward function
 safety_distance = 10
-w1 = 40.0
+w1 = 50.0
 w2 = 1.0
 w3 = 20.0
-w4 = 5.0
+w4 = 3.0
 
 #Hyperparameters
 learning_rate = 0.005
--- intersection_environment.py
+++ intersection_environment.py
@@ -74,11 +74,11 @@ env = gymnasium.make("custom-intersection-v0", render_mode="rgb_array", config={
 
 # #######################################
 # #Uncomment when training a new model
-# policy_kwargs = dict(net_arch=[64, 64], activation_fn=nn.ReLU)
+policy_kwargs = dict(net_arch=[64, 64], activation_fn=nn.ReLU)
 
-# model = DQN("MlpPolicy", env, policy_kwargs=policy_kwargs, learning_rate=config_intersection.learning_rate, buffer_size=config_intersection.buffer_size, learning_starts=config_intersection.learning_starts, batch_size=config_intersection.batch_size, gamma=config_intersection.gamma, train_freq=config_intersection.train_frequency, exploration_fraction=config_intersection.exploration_fraction, target_update_interval=config_intersection.target_update_interval)
-# model.learn(total_timesteps=config_intersection.total_timesteps, progress_bar=True)
-# model.save("DQN_Intersection_Model")
+model = DQN("MlpPolicy", env, policy_kwargs=policy_kwargs, learning_rate=config_intersection.learning_rate, buffer_size=config_intersection.buffer_size, learning_starts=config_intersection.learning_starts, batch_size=config_intersection.batch_size, gamma=config_intersection.gamma, train_freq=config_intersection.train_frequency, exploration_fraction=config_intersection.exploration_fraction, target_update_interval=config_intersection.target_update_interval)
+model.learn(total_timesteps=config_intersection.total_timesteps, progress_bar=True)
+model.save("DQN_Intersection_Model_Adjusted_Rear_to_3")
 # #######################################
 
 model = DQN.load("DQN_Intersection_Model", env=env)
