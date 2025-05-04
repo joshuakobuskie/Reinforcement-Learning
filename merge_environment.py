5ca175e5155a0344b39e60cfe408af7f598f7a53
joshuakobuskie <joshkobuskie@gmail.com>
2025-05-03 22:25:43 -0400

    feat: add speed tracking

--- merge_environment.py
+++ merge_environment.py
@@ -81,11 +81,11 @@ env = gymnasium.make("custom-merge-v0", render_mode="rgb_array", config={"other_
 
 ########################################
 # #Uncomment when training a new model
-policy_kwargs = dict(net_arch=[64, 64], activation_fn=nn.ReLU)
+# policy_kwargs = dict(net_arch=[64, 64], activation_fn=nn.ReLU)
 
-model = DQN("MlpPolicy", env, policy_kwargs=policy_kwargs, learning_rate=config_merge.learning_rate, buffer_size=config_merge.buffer_size, learning_starts=config_merge.learning_starts, batch_size=config_merge.batch_size, gamma=config_merge.gamma, train_freq=config_merge.train_frequency, exploration_fraction=config_merge.exploration_fraction, target_update_interval=config_merge.target_update_interval)
-model.learn(total_timesteps=config_merge.total_timesteps, progress_bar=True)
-model.save("DQN_Merge_Model")
+# model = DQN("MlpPolicy", env, policy_kwargs=policy_kwargs, learning_rate=config_merge.learning_rate, buffer_size=config_merge.buffer_size, learning_starts=config_merge.learning_starts, batch_size=config_merge.batch_size, gamma=config_merge.gamma, train_freq=config_merge.train_frequency, exploration_fraction=config_merge.exploration_fraction, target_update_interval=config_merge.target_update_interval)
+# model.learn(total_timesteps=config_merge.total_timesteps, progress_bar=True)
+# model.save("DQN_Merge_Model")
 ########################################
 
 model = DQN.load("DQN_Merge_Model", env=env)
